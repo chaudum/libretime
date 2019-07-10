06b9d9f6d94be9a2e04352893dc0c3fd5de704d9
Christian Haudum <christian.haudum@gmail.com>
2019-07-10 08:10:30 +0200

    [airtime-analyzer] Port setup.py to Python3

--- python_apps/airtime_analyzer/setup.py
+++ python_apps/airtime_analyzer/setup.py
@@ -1,56 +1,89 @@
-from setuptools import setup
-from subprocess import call
-import sys
 import os
+import sys
 
-# Change directory since setuptools uses relative paths
-script_path = os.path.dirname(os.path.realpath(__file__))
-print script_path
-os.chdir(script_path)
+from pathlib import Path
+from setuptools import setup
 
-# Allows us to avoid installing the upstart init script when deploying airtime_analyzer
-# on Airtime Pro:
-if '--no-init-script' in sys.argv:
-    data_files = []
-    sys.argv.remove('--no-init-script') # super hax
-else:
-    data_files = [('/etc/init', ['install/upstart/airtime_analyzer.conf']),
-                  ('/etc/init.d', ['install/sysvinit/airtime_analyzer'])]
-    print data_files
 
-setup(name='airtime_analyzer',
-      version='0.1',
-      description='Airtime Analyzer Worker and File Importer',
-      url='http://github.com/sourcefabric/Airtime',
-      author='Albert Santoni',
-      author_email='albert.santoni@sourcefabric.org',
-      license='MIT',
-      packages=['airtime_analyzer'],
-      scripts=['bin/airtime_analyzer'],
-      install_requires=[
-          'mutagen>=1.41.1', # got rid of specific version requirement 
-          'pika',
-          'daemon',
-          'file-magic',
-          'nose',
-          'coverage',
-          'mock',
-          'python-daemon==1.6',
-          'requests>=2.7.0',
-          'rgain',
-          # These next 3 are required for requests to support SSL with SNI. Learned this the hard way...
-          # What sucks is that GCC is required to pip install these. 
-          #'ndg-httpsclient',
-          #'pyasn1',
-          #'pyopenssl'
-      ],
-      zip_safe=False,
-      data_files=data_files)
+def change_directory():
+    """
+    Change into script directory since setuptools uses relative paths.
+    """
+    script_path = Path(os.path.dirname(__file__)).resolve()
+    os.chdir(script_path)
+    print(script_path)
 
-# Remind users to reload the initctl config so that "service start airtime_analyzer" works
-if data_files:
-    print "Remember to reload the initctl configuration"
-    print "Run \"sudo initctl reload-configuration; sudo service airtime_analyzer restart\" now."
-    print "Or on Ubuntu Xenial (16.04)"
-    print "Remember to reload the systemd configuration"
-    print "Run \"sudo systemctl daemon-reload; sudo service airtime_analyzer restart\" now."
+
+def get_data_files():
+    """
+    Return a list of data_files that should be installed in case setup.py is called
+    with the following commands: ``install``, ``install_data``, or ``develop``.
+    If the ``--no-install-script`` argument is provided, the function returns an
+    empty list and drops the argument from ``sys.argv``.
+    """
+    # Note: These files should be part of the LIbretime packaging,
+    # not of this Python package!
+    is_installation = sys.argv[1] in ["install", "install_data", "develop"]
+    install_init_scripts = "--no-init-script" not in sys.argv
+
+    if is_installation and install_init_scripts:
+        return [
+            ('/etc/init', ['install/upstart/airtime_analyzer.conf']),
+            ('/etc/init.d', ['install/sysvinit/airtime_analyzer']),
+        ]
+    if not install_init_scripts:
+        sys.argv.remove("--no-init-script")
+    return []
+
+
+def post_install():
+    """
+    Print instructions how to reload initclt configuration and restart airtime_analyzer
+    daemon.
+    This function should only be called **after** ``setup()`` and only if
+    ``data_files`` where provided.
+    """
+    print("---------------------")
+    print("Remember to reload the initctl configuration")
+    print("Run \"sudo initctl reload-configuration; sudo service airtime_analyzer restart\" now.")
+    print("Or on Ubuntu Xenial (16.04)")
+    print("Remember to reload the systemd configuration")
+    print("Run \"sudo systemctl daemon-reload; sudo service airtime_analyzer restart\" now.")
+    print("---------------------")
+
+
+if __name__ == "__main__":
+    change_directory()
+    data_files = get_data_files()
+    setup(
+        name='airtime_analyzer',
+        version='0.1',
+        description='Libretime Analyzer Worker and File Importer',
+        url='http://github.com/sourcefabric/Airtime',
+        author='Albert Santoni',
+        author_email='albert.santoni@sourcefabric.org',
+        license='MIT',
+        packages=['airtime_analyzer'],
+        scripts=['bin/airtime_analyzer'],
+        install_requires=[
+            'mutagen>=1.41.1',  # got rid of specific version requirement
+            'pika',
+            'daemon',
+            'file-magic',
+            'nose',
+            'coverage',
+            'mock',
+            'python-daemon',
+            'requests>=2.7.0',
+            'rgain',
+            # These next 3 are required for requests to support SSL with SNI. Learned this the hard way...
+            # What sucks is that GCC is required to pip install these
+            # 'ndg-httpsclient',
+            # 'pyasn1',
+            # 'pyopenssl'
+        ],
+        zip_safe=False,
+        data_files=data_files,
+    )
+    if data_files:
+        post_install()
